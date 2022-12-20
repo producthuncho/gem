a8d80eb47b10096e348ad27b36cff5c59e978d38
producthuncho <esixmanagementagency@gmail.com>
2022-12-20 03:32:57 +0100

    modified

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,18 +3,11 @@ const mongoose = require("mongoose");
 let mongoURI;
 
 if (process.env.NODE_ENV === "production") {
-<<<<<<< HEAD
     require('dotenv').config();
     mongoURI = process.env.DB_URL
 } else {
     require('dotenv').config();
     mongoURI = process.env.DB_URL;
-=======
-    mongoURI =
-        "mongodb+srv://Producthuncho:Delphine6718@cluster0.8o6e3pc.mongodb.net/?retryWrites=true&w=majority";
-} else {
-    mongoURI = "mongodb+srv://Producthuncho:Delphine6718@cluster0.8o6e3pc.mongodb.net/?retryWrites=true&w=majority";
->>>>>>> 77b44267b7ef3f5cc0b702bd24f894b97b27bb8a
 }
 
 module.exports = async function() {
